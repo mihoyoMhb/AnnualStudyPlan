f8b09f38a5ee2b1b65f91c3fc5d3a07a760e4946
mihoyoMhb <menghangb_ba@outlook.com>
2025-02-28 14:21:00 +0100

    nothing

---
+++ CMakeStudy/build/CMakeFiles/app.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hame0798/AnnualStudyPlan/CMakeStudy/add.cpp" "CMakeFiles/app.dir/add.cpp.o" "gcc" "CMakeFiles/app.dir/add.cpp.o.d"
+  "/home/hame0798/AnnualStudyPlan/CMakeStudy/div.cpp" "CMakeFiles/app.dir/div.cpp.o" "gcc" "CMakeFiles/app.dir/div.cpp.o.d"
+  "/home/hame0798/AnnualStudyPlan/CMakeStudy/main.cpp" "CMakeFiles/app.dir/main.cpp.o" "gcc" "CMakeFiles/app.dir/main.cpp.o.d"
+  "/home/hame0798/AnnualStudyPlan/CMakeStudy/mult.cpp" "CMakeFiles/app.dir/mult.cpp.o" "gcc" "CMakeFiles/app.dir/mult.cpp.o.d"
+  "/home/hame0798/AnnualStudyPlan/CMakeStudy/sub.cpp" "CMakeFiles/app.dir/sub.cpp.o" "gcc" "CMakeFiles/app.dir/sub.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
